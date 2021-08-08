27a10be016540ff4bb3f84034432820413c446cf
MatheusR42 <matheus.araujo@mercadolivre.com>
2021-08-08 17:29:47 -0300

    add test

--- deck/deck_test.go
+++ deck/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,20 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	const filename = "_decktesting"
+	os.Remove(filename)
+
+	deck := newDeck()
+	deck.saveToFile(filename)
+
+	loadedDeck := newDeckFromFile(filename)
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove(filename)
 }
